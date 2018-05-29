3631632a1da8f2066aa1d1fa810a031b53df600c
nezarsheikhi <nezarsheikhi@github.git>
2018-05-29 13:52:13 +0200

    Edit logo URL

--- js/ajax.js
+++ js/ajax.js
@@ -37,7 +37,7 @@ $(function(){
     });
           
     //OpenWeatherMap API key
-    var _0xffcc=["\x63\x33\x37\x37\x38\x31\x31\x62\x38\x38\x61\x39\x61\x32\x31\x63\x38\x61\x64\x31\x33\x36\x30\x39\x31\x65\x35\x38\x39\x63\x62\x65"];var OWM_API_KEY=_0xffcc[0]
+    var OWM_API_KEY = "c377811b88a9a21c8ad136091e589cbe";
     
     //display current city on load, or at least city of your IP address provider
     currentCity();
@@ -52,13 +52,13 @@ $(function(){
     //it might be wrong ofcourse, in my case it's wrong by 500km. But then you can type in your desired city
     //I might include navigator.geolocation in future, but it's often not available in browsers, so for now it's this
     function currentCity(){
-        var _0xf928=["\x68\x74\x74\x70\x73\x3A\x2F\x2F\x69\x70\x69\x6E\x66\x6F\x2E\x69\x6F\x3F\x74\x6F\x6B\x65\x6E\x3D\x37\x31\x33\x32\x62\x62\x34\x66\x31\x34\x30\x64\x63\x61","\x67\x65\x74\x4A\x53\x4F\x4E"];$[_0xf928[1]](_0xf928[0],callback)
+        $.getJSON("https://ipinfo.io?token=7132bb4f140dca", callback);
         
         function callback(data) {
             lat = data.loc.split(',')[0];
             lon = data.loc.split(',')[1];
     
-            OWM_URL = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&lang="+lang+"&units=metric&appid=" + OWM_API_KEY;
+            OWM_URL = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&lang="+lang+"&units=metric&appid=" + OWM_API_KEY;
             updateWeather(OWM_URL);
             
         }
@@ -87,7 +87,7 @@ $(function(){
             urlParam = "q="+city;
         }
         
-        OWM_URL = "https://api.openweathermap.org/data/2.5/weather?"+urlParam+"&lang="+lang+"&region="+country+"&units=metric&appid=" + OWM_API_KEY;
+        OWM_URL = "http://api.openweathermap.org/data/2.5/weather?"+urlParam+"&lang="+lang+"&region="+country+"&units=metric&appid=" + OWM_API_KEY;
         updateWeather(OWM_URL);
     }
 
@@ -97,7 +97,9 @@ $(function(){
         //weatherData will hold all requested data(typically object or array) in JSON format
         $.getJSON(OWM_URL, function(weatherData){
             
-            var _0x10a5=["\x68\x74\x74\x70\x73\x3A\x2F\x2F\x64\x6F\x6D\x61\x67\x6F\x6A\x70\x75\x6C\x6A\x69\x63\x2E\x6E\x65\x6F\x63\x69\x74\x69\x65\x73\x2E\x6F\x72\x67\x2F\x77\x65\x61\x74\x68\x65\x72\x5F\x70\x68\x6F\x74\x6F\x73\x2F\x75\x65\x6C\x6B\x6E\x76\x6B\x6E\x6A\x62\x62\x63\x62\x63\x64\x63\x62\x64\x6A\x64\x63\x6E\x75\x6E","\x69\x63\x6F\x6E","\x77\x65\x61\x74\x68\x65\x72","\x2E\x50\x4E\x47","\x62\x61\x63\x6B\x67\x72\x6F\x75\x6E\x64\x2D\x69\x6D\x61\x67\x65","\x75\x72\x6C\x28","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x64\x6F\x6D\x61\x67\x6F\x6A\x70\x75\x6C\x6A\x69\x63\x2E\x6E\x65\x6F\x63\x69\x74\x69\x65\x73\x2E\x6F\x72\x67\x2F\x77\x65\x61\x74\x68\x65\x72\x5F\x70\x68\x6F\x74\x6F\x73\x2F\x6A\x6E\x6B\x66\x76\x6E\x68\x75\x65\x6A\x63\x64\x66\x65\x72\x62\x76\x6E\x64\x5F","\x2E\x4A\x50\x45\x47","\x29","\x63\x73\x73","\x62\x6F\x64\x79"];var icon=_0x10a5[0]+ weatherData[_0x10a5[2]][0][_0x10a5[1]]+ _0x10a5[3];$(_0x10a5[10])[_0x10a5[9]](_0x10a5[4],_0x10a5[5]+ _0x10a5[6]+ weatherData[_0x10a5[2]][0][_0x10a5[1]]+ _0x10a5[7]+ _0x10a5[8])
+            var icon = "https://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/" + weatherData.weather[0].icon + ".png";
+
+            $("body").css("background-image", "url(" + "https://preview.ibb.co/f2uRVd/weather_" + weatherData.weather[0].icon + ".JPEG" + ")");
                 city = weatherData.name;
                 country = weatherData.sys.country;
                 windDir = degToCompass(weatherData.wind.deg % 360);
@@ -106,7 +108,7 @@ $(function(){
                 
                 pressure = weatherData.main.pressure;
                 clouds = weatherData.clouds.all;
-                $show.val(city+"'s MAP ⦿");
+                
                 $(".widget-menu__header").text(city + ", " + country);
                 $("#pressure").html(pressure + " hpa");
                 $("#humid").html(humid + "%");
@@ -126,7 +128,7 @@ $(function(){
 
                 
                 //Time zone function using lat and lon from update weather function
-                var _0x899a=["\x68\x74\x74\x70\x73\x3A\x2F\x2F\x61\x70\x69\x2E\x77\x6F\x72\x6C\x64\x77\x65\x61\x74\x68\x65\x72\x6F\x6E\x6C\x69\x6E\x65\x2E\x63\x6F\x6D\x2F\x70\x72\x65\x6D\x69\x75\x6D\x2F\x76\x31\x2F\x74\x7A\x2E\x61\x73\x68\x78\x3F\x71\x3D","\x2C","\x26\x66\x6F\x72\x6D\x61\x74\x3D\x6A\x73\x6F\x6E\x26\x6B\x65\x79\x3D\x61\x62\x35\x63\x61\x66\x63\x66\x64\x37\x30\x32\x34\x64\x38\x64\x38\x38\x66\x31\x31\x32\x36\x31\x31\x31\x38\x31\x30\x30\x34\x26\x63\x61\x6C\x6C\x62\x61\x63\x6B\x3D\x74\x66\x75\x6E\x63"];var url=_0x899a[0]+ lat+ _0x899a[1]+ lon+ _0x899a[2]
+                var url = "http://api.worldweatheronline.com/premium/v1/tz.ashx?q=" + lat + "," + lon + "&format=json&key=ab5cafcfd7024d8d88f112611181004&callback=tfunc";
                 //reference to jQuery library is required prior to using this function
                 $.getScript(url, function (response) { });
                 
@@ -253,7 +255,7 @@ $(function(){
         window.google = {};
         
         //api url
-        var _0x47d3=["\x68\x74\x74\x70\x73\x3A\x2F\x2F\x6D\x61\x70\x73\x2E\x67\x6F\x6F\x67\x6C\x65\x61\x70\x69\x73\x2E\x63\x6F\x6D\x2F\x6D\x61\x70\x73\x2F\x61\x70\x69\x2F\x6A\x73\x3F\x6B\x65\x79\x3D\x41\x49\x7A\x61\x53\x79\x44\x68\x31\x74\x72\x51\x5F\x68\x2D\x4A\x69\x42\x78\x69\x79\x34\x37\x6B\x52\x67\x5A\x76\x65\x48\x51\x59\x33\x50\x67\x70\x78\x79\x45\x26\x72\x65\x67\x69\x6F\x6E\x3D","\x26\x6C\x61\x6E\x67\x75\x61\x67\x65\x3D","\x26\x63\x61\x6C\x6C\x62\x61\x63\x6B\x3D\x3F"];var GGL_URL=_0x47d3[0]+ reve+ _0x47d3[1]+ leve+ _0x47d3[2]
+        var GGL_URL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDh1trQ_h-JiBxiy47kRgZveHQY3PgpxyE&region="+reve+"&language="+leve+"&callback=?";
         var pos= {lat: lati, lng: longi};
         var map = null, marker = null;
         
@@ -293,13 +295,12 @@ $(function(){
             if($map.is(':visible')) {
                 if(newLat != lat || newLon != lon) {
                     //update weather with new latitude and longitude
-                    OWM_URL = "https://api.openweathermap.org/data/2.5/weather?lat="+newLat+"&lon="+newLon+"&lang="+lang+"&units=metric&appid=" + OWM_API_KEY;
+                    OWM_URL = "http://api.openweathermap.org/data/2.5/weather?lat="+newLat+"&lon="+newLon+"&lang="+lang+"&units=metric&appid=" + OWM_API_KEY;
                     
                     updateWeather(OWM_URL);
                 }
                 $map.fadeOut();
-                // update the text inside the map button after click
-                $show.val(city+"'s MAP ⦿");
+                $show.val("MAP ⦿");
             }
             else {
                 $spin.fadeIn(100);
